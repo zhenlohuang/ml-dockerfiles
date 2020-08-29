b3d019b85d4c32bf50526e406b4d95a196ad642c
黄振龙 <huangzhenlong@cloudwalk.com>
2020-08-30 00:16:39 +0800

    Add ipywidgets to jupyterlab.

--- tvm-build/install/install-jupyterlab.sh
+++ tvm-build/install/install-jupyterlab.sh
@@ -4,4 +4,11 @@ set -e
 set -u
 set -o pipefail
 
-pip3 install notebook jupyterhub jupyterlab  
+# Install Jupyter Lab
+pip3 install notebook jupyterhub jupyterlab
+
+# Install extensions
+pip3 install ipywidgets
+
+# Enable extensions
+jupyter nbextension enable --py widgetsnbextension
